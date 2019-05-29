a605cea9ca11556c93433c9549b32ea8963c5abb
jxlwqq <jxlwqq@gmail.com>
2019-05-29 19:20:10 +0800

    first commit

---
+++ src/HelloWorld.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace Jxlwqq\HelloWorld;
+
+
+class HelloWorld
+{
+    public function say()
+    {
+        return 'Hello World';
+    }
+}
---
+++ test/HelloWorldTest.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Jxlwqq\HelloWorld\Test;
+
+
+use Jxlwqq\HelloWorld\HelloWorld;
+use PHPUnit\Framework\TestCase;
+
+class HelloWorldTest extends TestCase
+{
+    public function testSay()
+    {
+        $hello_world = new HelloWorld();
+        $this->assertEquals('Hello World', $hello_world->say());
+    }
+}
